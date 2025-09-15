b594d7bf0f7bb491cfd6bb435570612beeafa6d7
msiegelm <msiegelm@hawaii.edu>
2025-09-15 16:56:18 -0700

    adds frequency binning

--- src/smrover/nrt/RBRprocess.py
+++ src/smrover/nrt/RBRprocess.py
@@ -112,7 +112,8 @@ def dispr(h,freq):
 
     return kh,k
 
-def wave_energy_density(dep,dt,nperseg=None,noverlap=0,nsmooth=7,nfft=None,fftmethod="fft",window=None):
+
+def wave_energy_density(dep,dt,nperseg=None,noverlap=0,nsmooth=7,nfft=None,fftmethod="fft",window="hanning"):
     """
     Estimates 1-D Wave Energy Density Spectrum from pressure time series.
 
@@ -133,7 +134,10 @@ def wave_energy_density(dep,dt,nperseg=None,noverlap=0,nsmooth=7,nfft=None,fftme
         Length of boxcar smoothing window (default: 7). Must be odd integer or None (no smoothing).
     nfft : int or None
         Length of FFT (default: None, which uses nperseg)
-    
+    fftmethod : str
+        Method to compute FFT. Options are "fft" (default) or "welch"
+    window : str or None
+        Type of window to use if fftmethod = "fft". Options are "hanning" (default) or None (no windowing)
     Returns:
     -------
     out : Dict
@@ -153,47 +157,56 @@ def wave_energy_density(dep,dt,nperseg=None,noverlap=0,nsmooth=7,nfft=None,fftme
     t_det = signal.detrend(dep)
     if fftmethod == "fft":
         nt = len(t_det)
-        npositive = nt//2
-
-        pslice = slice(1,npositive)
-        ff = np.arange(1,npositive)/(nt*dt)
+        pslice, ff = one_sided_freqs(nt, fs)
         if window == "hanning":
             # Hanning Window
             x = np.arange(nt, dtype=float)
             phi = 2 * np.pi * x / nt
             weights = 0.5 * (1 - np.cos(phi))
-            ft = np.fft.fft(t_det*weights,n=nt)[pslice]
-        elif window is None:
-            ft = np.fft.fft(t_det,n=nt)[pslice]
-        ft2 = np.abs(ft) ** 2
-        ft2 *=2 #accounts for energy in negative frequencies
-        if window is not None:
-            psdnormfac = (dt/(weights**2).sum()) #normalization factor for window
         else:
-            psdnormfac = dt/nt
+            weights = np.ones(nt,dtype=float)
+        wnormfac = (weights**2).sum()/nt
+        ft = np.fft.fft(t_det*weights,n=nt)[pslice]
+        ft2 = np.abs(ft) ** 2
+        psdnormfac = (dt/(nt*wnormfac)) #normalization factor for window
         Spp = ft2 * psdnormfac
-        
+        #account for energy in negative frequencies
+        if nt % 2 == 0:
+            if Spp.size > 2:
+                Spp[1:-1] *= 2
+        else:
+            if Spp.size > 1:
+                Spp[1:] *= 2
     elif fftmethod == "welch":
         ff, Spp = signal.welch(t_det, fs=fs, nperseg=nperseg, noverlap=noverlap,return_onesided=True,nfft=nfft)
-        ff = ff[1:]
-        Spp = Spp[1:]
+    ##### Frequency Domain: Spectral smoothing
     if nsmooth is not None:
-        wconv = np.ones((nsmooth),dtype=float)/float(nsmooth)
-        Spp = np.convolve(Spp,wconv,mode='valid')
-        ff = np.convolve(ff,wconv,mode='valid')
-
+        kern = np.ones((nsmooth),dtype=float)/float(nsmooth)
+        ff = np.convolve(ff,kern,mode='same')
+        n_end = (nsmooth - 1) // 2  # to be chopped from each end
+        Sppc = np.convolve(Spp, kern, mode='same')
+        den = np.convolve(np.ones_like(Spp), kern, mode='same')
+        Spp = Sppc / den
+        Spp = Spp[n_end:-n_end]
+        ff = ff[n_end:-n_end]
+    ### pressure to surface elevation conversion
     h = np.nanmean(dep)
     kh, kk = dispr(h, ff)
     Kpp = np.cosh(kh)
     Kpp2 = Kpp**2
-    ###### does not amplify anything over cthresh to to prent from disproportionately amplifying noise floor at high freqs
+    ###### does not amplify anything over cthresh to prevnt disproportionate amplification of noise floor at high freqs
     cthresh = 20
     Kpp2[Kpp2 > cthresh] = cthresh
     Edens = Spp*Kpp2
+
+    ######
     out = {}
-    out['freq'] = ff
-    out['Spp'] = Spp
-    out['Edens'] = Edens
+    out['freq'] = ff[1:]
+    out['Spp'] = Spp[1:]
+    out['Edens'] = Edens[1:]
+    out['varts'] = np.var(t_det, ddof=0) 
+    out['trapzSpp'] = np.trapz(Spp,ff)
+    print('Parseval check: var(dep) = %f, int(Spp df) = %f' % (out['varts'],out['trapzSpp']))
     return out
 
 def calc_Hs(dep,dt,Tmin,Tmax,calcTp=False,cthresh=20):
@@ -302,3 +315,123 @@ def sig_waveheight(time,dep,dt,TrngSS,TrngIG,TrngSea,TrngSwell,cthresh=20):
     out.tnum = np.nanmean(dates.date2num(time),axis=0)
     out.t64 = num2dt64(out.tnum)
     return out
+
+
+### functions for binning wave energy density
+
+def bin_wave_energy_density(ff,Edens,fSS_min,fSS_max,fIG_min,fIG_max,dfSS,dfIG):
+    """
+    Defines frequency arrays and frequency bounds for wave energy density calculations.
+    Parameters:
+    -----------
+    fSS_min : float
+        Minimum frequency for swell band [Hz].
+    fSS_max : float
+        Maximum frequency for swell band [Hz].
+    fIG_min : float
+        Minimum frequency for infragravity band [Hz].
+    fIG_max : float
+        Maximum frequency for infragravity band [Hz].
+    dfSS : float
+        Frequency increment for swell band [Hz].
+    dfIG : float
+        Frequency increment for infragravity band [Hz].
+    Returns:
+    --------
+    fSS : array
+        Frequency array for swell band [Hz].
+    fbSS : array
+        Frequency bounds for swell band, shape (len(fSS)-1,2) [Hz].
+    fIG : array
+        Frequency array for infragravity band [Hz].
+    fbIG : array
+        Frequency bounds for infragravity band, shape (len(fIG)-1,2) [Hz].
+    FB : array
+        Combined frequency bounds, shape (nb,2) where nb is number of bins.
+    
+    """
+    iiSS_min = np.argmin(np.abs(ff-fSS_min))
+    iiSS_max = np.argmin(np.abs(ff-fSS_max))
+    fminSS = ff[iiSS_min]
+    fmaxSS = ff[iiSS_max]
+    fSS = np.arange(fminSS,fmaxSS+dfSS,dfSS)
+    fbSS = freqbounds(fSS)
+
+    iiIG_min = np.argmin(np.abs(ff-fIG_min))
+    iiIG_max = np.argmin(np.abs(ff-fIG_max))
+    fminIG = ff[iiIG_min]
+    fmaxIG = ff[iiIG_max]
+    fIG = np.arange(fminIG,fmaxIG,dfIG)
+    fbIG = freqbounds(fIG)
+
+    FB = np.vstack((fbIG,fbSS))
+
+    EdensB,ffB = WED_binned(Edens,ff,FB , method="sum")
+
+    df = FB[:,1] - FB[:,0]
+    print("Raw Energy Density: " , np.sum(Edens*(ff[1]-ff[0])))
+    print("Binned Energy Density: " , np.sum(EdensB*df))
+
+
+    return ffB,FB,EdensB
+
+def freqbounds(f):
+    """
+    Creates frequency bounds from frequency array.
+    Parameters:
+    -----------
+    f : array
+        Frequency array [Hz].
+    Returns:
+    --------
+    fbounds : array
+        Frequency bounds, shape (len(f)-1,2) [Hz].
+    
+    """
+    fbounds = np.zeros((len(f)-1,2))
+    for i in range(len(f)-1):
+        fbounds[i,0] = f[i]
+        fbounds[i,1] = f[i+1]
+    return fbounds
+
+def WED_binned(Edens,ff,fb,method="sum"):
+    """
+    Bin wave energy density spectrum onto frequency bins defined by fb.
+
+    Parameters:
+    -----------
+    Edens : array
+        Wave energy density spectrum [m^2/Hz].
+    ff : array
+        Frequencies corresponding to Edens [Hz].
+    fb : array
+        Frequency bin edges, shape (nb,2) where nb is number of bins.
+
+    method : str, optional
+        Method to compute binned energy density. Options are "sum" (default) or "trapz".
+    Returns:
+    --------
+    Edens_b : array
+        Binned wave energy density spectrum [m^2/Hz].
+    fmn : array
+        Mean frequency of each bin [Hz].
+    """
+    Edens_b = np.zeros(len(fb))*np.nan
+    nb,_ = np.shape(fb)
+    fmn = np.zeros(len(fb))*np.nan
+    for ii in range(nb):
+        fmin = fb[ii,0]
+        fmax = fb[ii,1]
+        dfo = ff[1] - ff[0]
+        kk = ((ff>=fmin) & (ff<fmax))
+        if method == "sum":
+            P = np.sum(Edens[kk]*dfo)
+        elif method == "trapz":
+            P = np.trapz(Edens[kk],ff[kk])
+
+        Edens_b[ii] = P / (fmax - fmin)  # average density in bin
+        fmn[ii] = np.nanmean(ff[kk])
+
+    return Edens_b,fmn
+
+    
